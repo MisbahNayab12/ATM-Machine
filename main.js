fe4dbe8f12b41fd690802ca313e98284a6ebb7e1
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2024-06-16 14:27:04 +0500

    Created ATM Machine

---
+++ main.js
@@ -0,0 +1,44 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 2022;
+let pinAns = await inquirer.prompt([
+    {
+        type: "number",
+        name: "pin",
+        message: "enter your pin code",
+    }
+]);
+if (pinAns.pin === myPin) {
+    console.log("Correct Pin Code!!!");
+    let operation = await inquirer.prompt([
+        {
+            type: "list",
+            name: "options",
+            message: "please select options",
+            choices: ["withdraw", "check balance"],
+        }
+    ]);
+    if (operation.options === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                type: "number",
+                name: "amount",
+                message: "enter your amount",
+            }
+        ]);
+        if (amountAns.amount > myBalance) {
+            console.log("Insufficient Balance");
+        }
+        else {
+            myBalance -= amountAns.amount;
+            console.log(`Your remaining balance is: ${myBalance}`);
+        }
+    }
+    else if (operation.options === "check balance") {
+        console.log(`Your current balance is: ${myBalance}`);
+    }
+}
+else {
+    console.log("Incorrect Pin Code. Try again!");
+}
